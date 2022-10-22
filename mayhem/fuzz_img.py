52cd3c26daed2467605a7ef87f33f10aab26b823
ennamarie19 <dallmeyerm@att.net>
2022-10-22 16:30:09 -0500

    Adjusted math and minimized testsuite

--- mayhem/fuzz_img.py
+++ mayhem/fuzz_img.py
@@ -1,28 +1,36 @@
 #! /usr/bin/python3
 import atheris
 import sys
-import io
+import math
+import logging
+
 from PIL import Image, UnidentifiedImageError
 
-with atheris.instrument_imports():
-    from primify.base import PrimeImage
+# with atheris.instrument_imports():
+from primify.base import PrimeImage
 
+log_2 = math.log(2)
 
+logging.disable(logging.CRITICAL)
 
 @atheris.instrument_func
 def TestOneInput(data):
-    fdp = atheris.FuzzedDataProvider(data)
-    byte_count = 4096
-    image_length = 2 ** fdp.ConsumeIntInRange(1, 3)
-    image_width = byte_count // image_length
+    if len(data) < 7 or len(data) > 512:
+        return -1
 
+    fdp = atheris.FuzzedDataProvider(data)
+    image_length = 2 ** fdp.ConsumeIntInRange(1, 3)
+    byte_count = 2 ** math.floor(math.log(fdp.remaining_bytes()) / log_2)
+    image_width = byte_count // image_length
+    print(image_width, image_length)
     try:
         image = Image.frombytes("L", (image_length, image_width), fdp.ConsumeBytes(byte_count))
-        resized_img = PrimeImage.resize_for_pixel_limit(image, fdp.ConsumeIntInRange(128, 4095))
+        resized_img = PrimeImage.resize_for_pixel_limit(image, byte_count - 1)
         quant_img = PrimeImage.quantize_image(resized_img)
         PrimeImage.quantized_image_to_number(quant_img)
     except (UnidentifiedImageError, ValueError) as e:
-        if isinstance(e, ValueError) and "magic number" not in str(e) and "tile cannot" not in str(e) and "not enough" not in str(e):
+        if isinstance(e, ValueError) and "magic number" not in str(e) and "tile cannot" not in str(
+                e) and "not enough" not in str(e):
             raise
 
 
