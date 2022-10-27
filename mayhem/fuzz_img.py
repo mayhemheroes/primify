717ae8737c13b05ff9e4203d0dc515e6c37d392e
ennamarie19 <dallmeyerm@att.net>
2022-10-27 16:02:16 -0500

    Removed print

--- mayhem/fuzz_img.py
+++ mayhem/fuzz_img.py
@@ -22,13 +22,12 @@ def TestOneInput(data):
     image_length = 2 ** fdp.ConsumeIntInRange(1, 3)
     byte_count = 2 ** math.floor(math.log(fdp.remaining_bytes()) / log_2)
     image_width = byte_count // image_length
-    print(image_width, image_length)
     try:
         image = Image.frombytes("L", (image_length, image_width), fdp.ConsumeBytes(byte_count))
         resized_img = PrimeImage.resize_for_pixel_limit(image, byte_count - 1)
         quant_img = PrimeImage.quantize_image(resized_img)
         PrimeImage.quantized_image_to_number(quant_img)
-    except (UnidentifiedImageError, ValueError) as e:
+    except (ZeroDivisionError, UnidentifiedImageError, ValueError) as e:
         if isinstance(e, ValueError) and "magic number" not in str(e) and "tile cannot" not in str(
                 e) and "not enough" not in str(e):
             raise
